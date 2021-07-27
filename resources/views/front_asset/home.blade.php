c90b3214b0f4f865554aa19476281f2272ee0d39
Lifestream-s <agustinus2h@gmail.com>
2021-07-27 16:10:06 +0700

    Perubahan sistem
    Hilangkan pesan diganti peminjaman dan pengembalian

--- resources/views/front_asset/home.blade.php
+++ resources/views/front_asset/home.blade.php
@@ -2,6 +2,12 @@
     @section('title','Perpustakaan')
     
     @section('konten')
-    <p>Oke</p>
+        <div class="jumbotron mt-5">
+          <h1 class="display-4">Perpustakaan V1</h1>
+          <p class="lead">Informasi megenai buku yang dipinjam, jangka waktu peminjaman dapat di lihat pada detail buku.</p>
+          <hr class="my-4">
+          <p>Revisi program dapat dikirim melalui email, dan segera laporkan testing error pada aplikasi ini</p>
+          <a class="btn btn-primary btn-lg" href="#" role="button">Check Data</a>
+        </div>
     @endsection
     
