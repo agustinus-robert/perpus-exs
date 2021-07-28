7b116e606697647aba663f72a1f91ee031a3debd
Lifestream-s <agustinus2h@gmail.com>
2021-07-28 09:14:03 +0700

    Halaman daftar buku integrasikan dengan datatables

--- resources/views/buku/index.blade.php
+++ resources/views/buku/index.blade.php
@@ -10,7 +10,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav mx-auto">
                     <li class="nav-item active">
-                      <a class="nav-link" href="#">Daftar Buku</a>
+                      <a class="nav-link" href="{{url('/getDaftarBuku')}}">Daftar Buku</a>
                     </li>
                     <li class="nav-item">
                       <a class="nav-link" href="{{url('/buku-add')}}">Tambah Buku</a>
