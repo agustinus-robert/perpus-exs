325743264acfc45fa891c7dc49983923638c8b4f
Lifestream-s <agustinus2h@gmail.com>
2021-07-31 18:21:49 +0700

    Tampilan peminjaman buku dan alur program
    1. Pertama jika belum ada buku pada list, maka set qty = 1
    2. Jika sudah ada data di list maka increment qty, jadi qty++
    3. Setiap data di list diurutkan berdasarkan id buku
    4. Terdapat field akutal buku, jadi saat pinjam bisa tau jumlah buku di field kanan, jumlah tersimpannya

--- app/Http/Livewire/Peminjaman/PeminjamLivewireTrans.php
+++ app/Http/Livewire/Peminjaman/PeminjamLivewireTrans.php
@@ -1,23 +1,67 @@
 <?php
 
 namespace App\Http\Livewire\Peminjaman;
-
+use Illuminate\Support\Facades\DB;
+use App\bukuModel as bM;
+use Session;
 use Livewire\Component;
 
 class PeminjamLivewireTrans extends Component
 {
     public $buku_pilih;
+    public $isbn;
+    public $cari;
+  //  public $jml_pjm;
+    public $data_buku = [];
     
     public function masukTransPinjam(){
         dd($this->buku_pilih);
     }
     
     public function get_id_buku($id){
-        dd($id);
+//        $get = bM::select("*")->where('id', $id)
+//                  ->get()->toArray();
+        
+          $get = bM::select(DB::raw('SUM(lib_jumlah_buku.jumlah_buku) as jml_buku,'
+                 . 'lib_buku.judul as judul, '
+                 . 'lib_buku.pengarang as pengarang, '
+                 . 'lib_buku.penerbit as penerbit, '
+                 . 'lib_buku.isbn as isbn, '
+                 . 'lib_supplier.nama_supplier as nama_supplier, '
+                 . 'lib_jumlah_buku.id as id, '
+                 . 'lib_buku.id as awal_id'
+                ))
+              ->leftJoin('lib_jumlah_buku', 'lib_buku.id', '=', 'lib_jumlah_buku.id_buku')
+              ->leftJoin('lib_supplier', 'lib_jumlah_buku.supplier_id', '=', 'lib_supplier.id')
+              ->where('lib_buku.id', $id)
+              ->groupBy('lib_buku.id') 
+              ->get()->toArray();
+         
+        foreach($get as $k => $v){      
+            
+            if(empty($this->data_buku[$id])){
+                $this->data_buku[$v['awal_id']][]= array('id' => $v['id'],
+                    'judul' => $v['judul'],
+                    'jml_pinjam' => 1,
+                    'jml_stock' => $v['jml_buku']);
+            } else {
+                if($this->data_buku[$v['awal_id']] == $this->data_buku[$id]){
+                    $qty = 1;
+                    foreach($this->data_buku[$v['awal_id']] as $k => $vss){
+                        $this->data_buku[$id][$k]['jml_pinjam']++;
+                    }
+                }
+            }
+        }
+    }
+    
+    public function cari_click(){
+        
     }
     
     public function render()
     {
-        return view('livewire.peminjaman.peminjam-livewire-trans');
+        $data['trans'] = bM::select("*")->get()->toArray();
+        return view('livewire.peminjaman.peminjam-livewire-trans', $data);
     }
 }
