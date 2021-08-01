3793f49a42007ee572174dfb33c6e6fd09dc95a5
Lifestream-s <agustinus2h@gmail.com>
2021-08-01 20:22:10 +0700

    Kembalikan plugins datatables

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -13,6 +13,9 @@
         <link href="https://cdn.datatables.net/1.10.25/css/dataTables.bootstrap5.min.css" rel="stylesheet">
        
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <script src="https://code.jquery.com/jquery-3.5.1.js"></script> 
+        <script src="https://cdn.datatables.net/1.10.25/js/jquery.dataTables.min.js"></script>
+         <script src="https://cdn.datatables.net/1.10.25/js/dataTables.bootstrap5.min.js"></script>
      
         @livewireStyles
     </head>
