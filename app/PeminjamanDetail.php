aa982e5998cadedf7153b40bd7cfbee0bfae638b
Lifestream-s <agustinus2h@gmail.com>
2021-08-01 22:42:21 +0700

    Ketentuan masuk di tabel pinjam dan detail_peminjaman
    1. Jika transaksi sudah di create di tabel pinjam, maka ambil last_insert_id pada transaksi
    2. Jika sudah mendapat last_insert_id transaksi di tabel pinjam, maka bentuk data array_push di detail_peminjaman
    
    Keterangan
    
    Posisi tabel lib_pinjam adalah header tabel yang menyimpan 1 nomor ternsaksi, sedangkan detail_pinjaman menyimpan barang dan id_transaksi tetap, tapi id_transaksi baris buku yang disimpan

---
+++ app/PeminjamanDetail.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PeminjamanDetail extends Model
+{
+    //
+    protected $table = "lib_peminjaman_detail";
+    public $timestamps = false;
+    protected $guarded = [];
+}
