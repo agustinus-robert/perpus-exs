719ecc339f92f87d780ec11286771b9334b11244
Lifestream-s <agustinus2h@gmail.com>
2021-08-03 20:05:06 +0700

    Hapus lib_pemembalian, tambahkan status pengembalian di lib_peminjaman, jika 1 barang sudah keluar, jika 2 barang sudah dikembalikan

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100420
  File Encoding         : 65001
 
- Date: 27/07/2021 16:20:28
+ Date: 03/08/2021 20:04:13
 */
 
 SET NAMES utf8mb4;
@@ -28,13 +28,18 @@ CREATE TABLE `lib_buku` (
   `penerbit` varchar(60) DEFAULT NULL,
   `tahun_terbit` int(11) DEFAULT NULL,
   `isbn` char(13) DEFAULT NULL,
+  `foto` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of lib_buku
 -- ----------------------------
 BEGIN;
+INSERT INTO `lib_buku` VALUES (1, 'Aku', 'Chairil anwar', 'Gramedia Solo', NULL, '1234567891012', 'chairil.jpg');
+INSERT INTO `lib_buku` VALUES (3, 'Steve Jobs', 'Walter Issacson', 'gramedia', NULL, '2345678910111', NULL);
+INSERT INTO `lib_buku` VALUES (4, 'the broken wings', 'Khalil Gibran', 'Gramedia', NULL, '345678910113', NULL);
+INSERT INTO `lib_buku` VALUES (7, 'Das Kapital', 'Karl Mark', 'Gramedia', NULL, '1234567891011', 'karl.jpg');
 COMMIT;
 
 -- ----------------------------
@@ -56,43 +61,78 @@ BEGIN;
 COMMIT;
 
 -- ----------------------------
+-- Table structure for lib_jumlah_buku
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_jumlah_buku`;
+CREATE TABLE `lib_jumlah_buku` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_buku` int(11) DEFAULT NULL,
+  `jumlah_buku` int(11) DEFAULT NULL,
+  `supplier_id` int(11) DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of lib_jumlah_buku
+-- ----------------------------
+BEGIN;
+INSERT INTO `lib_jumlah_buku` VALUES (1, 1, 2, 1, '2021-07-29 15:22:02', '2021-07-29 15:22:08');
+INSERT INTO `lib_jumlah_buku` VALUES (2, 1, 4, 3, '2021-07-29 08:21:49', '2021-07-29 08:21:49');
+INSERT INTO `lib_jumlah_buku` VALUES (3, 7, 10, 1, '2021-07-29 08:53:23', '2021-07-29 08:53:23');
+COMMIT;
+
+-- ----------------------------
 -- Table structure for lib_peminjam
 -- ----------------------------
 DROP TABLE IF EXISTS `lib_peminjam`;
 CREATE TABLE `lib_peminjam` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_pengunjung` int(11) DEFAULT NULL,
-  `id_buku` int(11) DEFAULT NULL,
   `tanggal_pinjam` date DEFAULT NULL,
   `tanggal_kembali` date DEFAULT NULL,
-  `kode_pinjam` varchar(6) DEFAULT NULL,
+  `status` int(11) DEFAULT 0,
+  `pengembalian` int(11) DEFAULT NULL COMMENT 'jika 0 maka barang keluar, jika 1 barang sudah kembali',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of lib_peminjam
 -- ----------------------------
 BEGIN;
+INSERT INTO `lib_peminjam` VALUES (1, 1, '2021-08-03', '2021-08-06', 1, NULL);
+INSERT INTO `lib_peminjam` VALUES (2, 2, '2021-08-03', '2021-08-06', 1, NULL);
+INSERT INTO `lib_peminjam` VALUES (3, 1, '2021-08-03', '2021-08-07', 1, NULL);
+INSERT INTO `lib_peminjam` VALUES (4, 3, '2021-08-03', '2021-08-07', 1, NULL);
+INSERT INTO `lib_peminjam` VALUES (5, 1, '2021-08-03', '2021-08-05', 1, NULL);
+INSERT INTO `lib_peminjam` VALUES (6, 2, NULL, NULL, 0, NULL);
 COMMIT;
 
 -- ----------------------------
--- Table structure for lib_pengembalian
+-- Table structure for lib_peminjaman_detail
 -- ----------------------------
-DROP TABLE IF EXISTS `lib_pengembalian`;
-CREATE TABLE `lib_pengembalian` (
+DROP TABLE IF EXISTS `lib_peminjaman_detail`;
+CREATE TABLE `lib_peminjaman_detail` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_anggota` int(11) DEFAULT NULL,
-  `tanggal_pinjam` date DEFAULT NULL,
-  `tanggal_kembali` date DEFAULT NULL,
-  `kode_pinjam` varchar(6) DEFAULT NULL,
-  `kode_kembali` varchar(6) DEFAULT NULL,
+  `id_trans_pinjam` int(11) DEFAULT NULL,
+  `id_buku` int(11) DEFAULT NULL,
+  `jumlah_pinjam` int(11) DEFAULT NULL,
+  `jumlah_aktual` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
--- Records of lib_pengembalian
+-- Records of lib_peminjaman_detail
 -- ----------------------------
 BEGIN;
+INSERT INTO `lib_peminjaman_detail` VALUES (1, 1, 1, 1, 6);
+INSERT INTO `lib_peminjaman_detail` VALUES (2, 1, 3, 1, 10);
+INSERT INTO `lib_peminjaman_detail` VALUES (3, 2, 3, 1, 6);
+INSERT INTO `lib_peminjaman_detail` VALUES (4, 3, 3, 1, 6);
+INSERT INTO `lib_peminjaman_detail` VALUES (5, 4, 7, 1, 10);
+INSERT INTO `lib_peminjaman_detail` VALUES (6, 5, 7, 4, 10);
+INSERT INTO `lib_peminjaman_detail` VALUES (7, 6, 1, 1, 6);
 COMMIT;
 
 -- ----------------------------
@@ -106,12 +146,35 @@ CREATE TABLE `lib_pengunjung` (
   `tgl_lahir` date DEFAULT NULL,
   `alamat` varchar(70) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 -- ----------------------------
 -- Records of lib_pengunjung
 -- ----------------------------
 BEGIN;
+INSERT INTO `lib_pengunjung` VALUES (1, 'robert', '123456', '2021-07-14', 'solo');
+INSERT INTO `lib_pengunjung` VALUES (2, 'maria', '89994343', '2021-07-21', 'Solo');
+INSERT INTO `lib_pengunjung` VALUES (3, 'Fera', '3424342', '2021-07-13', 'Solo');
+COMMIT;
+
+-- ----------------------------
+-- Table structure for lib_supplier
+-- ----------------------------
+DROP TABLE IF EXISTS `lib_supplier`;
+CREATE TABLE `lib_supplier` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_supplier` varchar(50) DEFAULT NULL,
+  `kode_supplier` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of lib_supplier
+-- ----------------------------
+BEGIN;
+INSERT INTO `lib_supplier` VALUES (1, 'PT buku 1', 12345);
+INSERT INTO `lib_supplier` VALUES (2, 'PT buku 2', 23456);
+INSERT INTO `lib_supplier` VALUES (3, 'PT buku 3', 34567);
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
