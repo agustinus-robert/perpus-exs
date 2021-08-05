cbdacb75ad00c66f48b73f6f4a1280d66192a3c5
Lifestream-s <agustinus2h@gmail.com>
2021-08-05 19:44:44 +0700

    Ketentuan Denda
    1. Jika -1 adalah belum memilih denda
    2. Jika 1 adalah keterlambatan mengembalikan buku
    3. Jika 2 adalah buku hilang atau rusak
    
    <Dipilih melalui select>

--- app/Http/Livewire/Denda/DendaLivewire.php
+++ app/Http/Livewire/Denda/DendaLivewire.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace App\Http\Livewire\Denda;
-
+use Illuminate\Support\Facades\DB;
+use App\bukuModel as bM;
+use App\Peminjaman as Pjm;
+use App\PeminjamanDetail as PjmDtl;
+use Session;
 use Livewire\Component;
 
 class DendaLivewire extends Component
 {
     public $ids;
+    public $jml_denda;
     
     public function mount($id){
         $this->ids = $id;
@@ -14,6 +19,21 @@ class DendaLivewire extends Component
     
     public function render()
     {
-        return view('livewire.denda.denda-livewire');
+        $data['daftar_proses'] = Pjm::select(DB::raw('lib_pengunjung.nama as nama,'
+                    .'lib_peminjam.tanggal_pinjam as tgl_pjm, '
+                    .'lib_peminjam.tanggal_kembali as tgl_kbl, '
+                    .'lib_peminjam.id as id, '
+                    .'lib_peminjaman_detail.id_buku as det_buku,'
+                    .'lib_peminjaman_detail.jumlah_pinjam as jml_buku, '
+                    .'lib_buku.judul as nama_buku '
+                ))  
+              ->leftJoin('lib_pengunjung', 'lib_peminjam.id_pengunjung', '=', 'lib_pengunjung.id')
+              ->leftJoin('lib_peminjaman_detail','lib_peminjam.id','=','lib_peminjaman_detail.id_trans_pinjam')
+              ->leftJoin('lib_buku','lib_peminjaman_detail.id_buku','=','lib_buku.id')
+             // ->groupBy('lib_peminjam.id')
+              ->where('lib_peminjam.id',$this->ids)
+              ->get()->toArray();
+        
+        return view('livewire.denda.denda-livewire', $data);
     }
 }
--- resources/views/livewire/denda/denda-livewire.blade.php
+++ resources/views/livewire/denda/denda-livewire.blade.php
@@ -1,3 +1,71 @@
 <div>
+    <div class='row p-4'>
+        <h2 class='text-center'>Kelola Denda</h2>
+        <hr>
+        <div class='row justify-content-center'>
+            <nav class="col-md-6 navbar navbar-expand-lg navbar-light bg-light">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mx-auto">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="{{url('/getDaftarBuku')}}">Laporan Deda</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="{{url('/transaksi-denda')}}">Transaksi Denda</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+        </div>
+    </div>
     
+    <div class="row">    
+        <form wire:submit.prevent="masukProsesTransaksi">
+            <div class="col-md-8 p-3 mx-auto">
+                <table class="table">
+                    <tr>
+                        <td><label>Tanggal Awal: <input type="date" wire:model="waktu_awal" /></label></td>
+                        <td><label>Tanggal Akhir: <input type="date" wire:model="waktu_akhir" /></label></td>
+                    </tr>
+                </table>
+            </div>
+            
+            <div class="col-md-12">
+                <table class="table text-center">
+                    <thead>
+                        <tr>
+                            <th>No</th>
+                            <th>Nama Buku</th>
+                            <th>Jumlah Pinjam</th>
+                            <th>Status Pengembalian</th>
+                        </tr>
+                    </thead>
+                <?php
+                    $i = 1;
+                    foreach($daftar_proses as $k => $v){
+                ?>
+                    <tr>
+                        <td>{{$i++}}</td>
+                        <td>{{$v['nama_buku']}}</td>
+                        <td>{{$v['jml_buku']}}</td>
+                        <td><select wire:model="">
+                                <option value="-1">Pilih Denda</option>
+                                <option value="1">Keterlambatan</option>
+                                <option value="2">Buku Hilang/Rusak</option>
+                            </select>
+                        </td>
+                    </tr>
+                <?php } ?>
+
+                </table>
+                
+                <div class="col text-center mb-3">
+                    <label>Jumlah Denda: <input type="text" class="form-control" wire:model="jml_denda" placeholder="Massukan nominal Denda" /></label>
+                </div>
+            </div>
+            
+            <div class="col text-center">
+                <button type="submit" class="btn btn-success mt-3" />Proses Data</button>
+            </div>
+        </form>
+    </div>
 </div>
